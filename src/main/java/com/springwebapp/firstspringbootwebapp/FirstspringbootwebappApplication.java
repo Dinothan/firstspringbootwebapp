b87d5453b5621514a84f7e41bc5b3544be38eba0
Aux-084 <dinothan1@gmail.com>
2018-10-08 16:45:27 +0530

    ccc

---
+++ src/main/java/com/springwebapp/firstspringbootwebapp/FirstspringbootwebappApplication.java
@@ -0,0 +1,12 @@
+package com.springwebapp.firstspringbootwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstspringbootwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstspringbootwebappApplication.class, args);
+	}
+}
---
+++ src/test/java/com/springwebapp/firstspringbootwebapp/FirstspringbootwebappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springwebapp.firstspringbootwebapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FirstspringbootwebappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
